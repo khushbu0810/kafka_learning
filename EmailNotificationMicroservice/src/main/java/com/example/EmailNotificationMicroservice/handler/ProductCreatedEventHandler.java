1f47251f0b9540c6655a905e44a19eadd1f63211
Khushbu <Kahlwt08@gmail.com>
2026-06-14 12:45:07 +0530

    kafka consumer initialization

---
+++ EmailNotificationMicroservice/src/main/java/com/example/EmailNotificationMicroservice/handler/ProductCreatedEventHandler.java
@@ -0,0 +1,22 @@
+package com.example.EmailNotificationMicroservice.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@KafkaListener(topics = "product-created-events-topic")
+/*
+    this method should be invoked whenever a new message is received from specified kafka topic
+    kafkaListener(topic1,topic2....)
+    */
+public class ProductCreatedEventHandler {
+
+    @KafkaHandler
+    public void handle(ProductCreatedEvent productCreatedEvent) {
+        log.info("Received new event: "+ productCreatedEvent.getTitle());
+
+    }
+}
