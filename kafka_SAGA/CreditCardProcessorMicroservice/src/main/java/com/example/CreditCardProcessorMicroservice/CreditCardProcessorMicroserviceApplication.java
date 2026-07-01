95d15fad55b83da4b9d7f47b5ba7d764ef556d96
Khushbu <Kahlwt08@gmail.com>
2026-07-01 13:21:03 +0530

    kafka Saga

---
+++ kafka_SAGA/CreditCardProcessorMicroservice/src/main/java/com/example/CreditCardProcessorMicroservice/CreditCardProcessorMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.CreditCardProcessorMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardProcessorMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardProcessorMicroserviceApplication.class, args);
+	}
+
+}
---
+++ kafka_SAGA/ProductMicroservice/src/main/java/com/example/ProductMicroservice/config/KafkaConfig.java
@@ -0,0 +1,10 @@
+package com.example.ProductMicroservice.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaConfig {
+
+    private String productsEventsTopicName;
+
+}
